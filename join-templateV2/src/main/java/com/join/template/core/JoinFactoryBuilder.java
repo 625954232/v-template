e4e59f2c228a099c9b7e65126a38b260964f4f43
caoyou <625954232@qq.com>
2019-08-07 14:46:59 +0800

    修改结构

--- join-templateV2/src/main/java/com/join/template/core/JoinFactoryBuilder.java
+++ join-templateV2/src/main/java/com/join/template/core/JoinFactoryBuilder.java
@@ -5,10 +5,16 @@ import com.join.template.configuration.ExprConfig;
 import com.join.template.factory.JoinFactory;
 import com.join.template.factory.JoinFactoryBase;
 import com.join.template.factory.template.TemplateFactory;
+import com.join.template.listener.ParserListener;
+import com.join.template.listener.ProcessListener;
+import com.join.template.parser.Parser;
 import com.join.template.process.Process;
+
+import java.util.List;
 
 
 public class JoinFactoryBuilder {
+
 
     JoinFactory joinFactory;
 
@@ -24,6 +30,44 @@ public class JoinFactoryBuilder {
         this.joinFactory = new JoinFactoryBase(configuration);
     }
 
+    /**
+     * 新增模版工厂
+     *
+     * @param nodeType        节点类型
+     * @param templateFactory 模版工厂
+     * @return
+     */
+    public JoinFactory addFactory(String nodeType, TemplateFactory templateFactory) {
+        return joinFactory.addFactory(nodeType, templateFactory);
+    }
+
+    /**
+     * 新增表达式配置
+     *
+     * @param nodeType
+     * @param tag
+     * @param parser
+     * @param process
+     * @return
+     */
+    public JoinFactory addExprConfig(Integer nodeType, String tag, Parser parser, Process process) {
+        return joinFactory.addExprConfig(nodeType, tag, parser, process);
+    }
+
+    /**
+     * 新增表达式配置
+     *
+     * @param nodeType
+     * @param tag
+     * @param parser
+     * @param process
+     * @param parserListeners
+     * @param processListeners
+     * @return
+     */
+    public JoinFactory addExprConfig(Integer nodeType, String tag, Parser parser, Process process, List<ParserListener> parserListeners, List<ProcessListener> processListeners) {
+        return joinFactory.addExprConfig(nodeType, tag, parser, process, parserListeners, processListeners);
+    }
 
     /**
      * 缓存模板
